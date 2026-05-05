05f30b2b2cbe988fe36af7c01bcc5aaa33091af6
omkar9 <omkar946@gmail.com>
2026-05-05 17:31:28 +0530

    feat: add StreakTrackApp.kt

---
+++ app/src/main/java/com/androidforge/streaktrack/StreakTrackApp.kt
@@ -0,0 +1,15 @@
+package com.androidforge.streaktrack
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class StreakTrackApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
