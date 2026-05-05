d0a591852aad7d38f7f043ab49be3e239a14804d
omkar9 <omkar946@gmail.com>
2026-05-05 17:30:57 +0530

    feat: add Constants.kt

---
+++ app/src/main/java/com/androidforge/streaktrack/core/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androidforge.streaktrack.core.util
+
+object Constants {
+    const val DATABASE_NAME = "streak_track_db"
+    const val DATASTORE_NAME = "streak_track_preferences"
+
+    // WorkManager tags
+    const val REMINDER_WORK_TAG = "habit_reminder_work"
+}
