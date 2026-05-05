f68e77eb12172b01f71b702b16795f813ca3efbf
omkar9 <omkar946@gmail.com>
2026-05-05 17:30:55 +0530

    feat: add AdConstants.kt

---
+++ app/src/main/java/com/androidforge/streaktrack/core/util/AdConstants.kt
@@ -0,0 +1,10 @@
+package com.androidforge.streaktrack.core.util
+
+object AdConstants {
+    // Test Ad unit IDs provided by AdMob documentation
+    const val BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111"
+    const val INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712"
+
+    // Test AdMob Application ID provided by AdMob documentation
+    const val ADMOB_APP_ID = "ca-app-pub-3940256099942544~3347511713"
+}
