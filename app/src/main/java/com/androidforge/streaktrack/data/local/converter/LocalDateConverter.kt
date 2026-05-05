fe76d2f6483d5c7fff58cfce10af5da04bab82ff
omkar9 <omkar946@gmail.com>
2026-05-05 17:30:40 +0530

    feat: add LocalDateConverter.kt

---
+++ app/src/main/java/com/androidforge/streaktrack/data/local/converter/LocalDateConverter.kt
@@ -0,0 +1,16 @@
+package com.androidforge.streaktrack.data.local.converter
+
+import androidx.room.TypeConverter
+import java.time.LocalDate
+
+class LocalDateConverter {
+    @TypeConverter
+    fun fromLocalDate(date: LocalDate?): String? {
+        return date?.toString()
+    }
+
+    @TypeConverter
+    fun toLocalDate(dateString: String?): LocalDate? {
+        return dateString?.let { LocalDate.parse(it) }
+    }
+}
